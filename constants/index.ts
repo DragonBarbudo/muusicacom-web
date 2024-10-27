da2b5cf50a61f8adb9b3a45de50f660bf486ae6b
Albin Rodriguez <ealbinu@gmail.com>
2024-10-27 16:08:14 -0600

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const appName = 'Muusica.com'
+export const appDescription = 'EL SOUNDTRACK DE TU VIDA | Creamos tu canción personalizada'
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const online = useOnline()
+</script>
+
+<template>
+  <div>
+    <MuusicaLogo w-60 mx-auto />
+    <Logos mb-6 />
+    <Suspense>
+      <ClientOnly>
+        <PageView v-if="online" />
+        <div v-else text-gray:80>
+          You're offline
+        </div>
+      </ClientOnly>
+      <template #fallback>
+        <div italic op50>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    <InputEntry />
+  </div>
+</template>
